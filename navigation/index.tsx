7fe10ace805b1867790561ef6086ca68c434339b
SangaHub <sangamesh3311@gmail.com>
2023-02-07 22:36:34 -0800

    Second commit

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -71,7 +71,7 @@ function BottomTabNavigator() {
         name="Home"
         component={TabOneScreen}
         options={{
-          title: 'Home tab',
+          title: 'Home tab check',
           tabBarIcon: ({ color }) => <Ionicons name="home" color={color} size= {20}/>,
         }}
       />
